e2afd9af669abf70a4eb394e672ba206adb7ce5f
Dmitry Konishchev <konishchev@gmail.com>
2024-01-01 17:52:32 +0300

    Introduce ResultTools

--- src/core.rs
+++ src/core.rs
@@ -6,3 +6,25 @@ pub type GenericError = Box<dyn ::std::error::Error + Send + Sync>;
 macro_rules! Err {
     ($($arg:tt)*) => (::std::result::Result::Err(format!($($arg)*).into()))
 }
+
+pub trait ResultTools {
+    type Value;
+    type Error;
+
+    fn ok_or_handle_error<F: FnOnce(Self::Error)>(self, op: F) -> Option<Self::Value>;
+}
+
+impl<T, E> ResultTools for Result<T, E> {
+    type Value = T;
+    type Error = E;
+
+    fn ok_or_handle_error<F: FnOnce(Self::Error)>(self, op: F) -> Option<Self::Value> {
+        match self {
+            Ok(value) => Some(value),
+            Err(err) => {
+                op(err);
+                None
+            }
+        }
+    }
+}
