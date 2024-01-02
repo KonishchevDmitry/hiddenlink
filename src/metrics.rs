7dad341290414e99792747dfad33c794dd3e6b1c
Dmitry Konishchev <konishchev@gmail.com>
2024-01-02 16:51:19 +0300

    Introduce metrics module

---
+++ src/metrics.rs
@@ -0,0 +1,59 @@
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use axum::{
+    Router,
+    extract::{State, Request},
+    http::{StatusCode, header},
+    middleware::{self, Next},
+    response::IntoResponse, response::Response,
+    routing::get,
+};
+use log::{trace, info, error};
+use prometheus_client::registry::Registry;
+use tokio::net::TcpListener;
+use tokio::task::JoinHandle;
+
+use crate::core::GenericResult;
+
+pub async fn run(bind_address: SocketAddr, registry: Registry) -> GenericResult<JoinHandle<std::io::Result<()>>> {
+    let listener = TcpListener::bind(bind_address).await.map_err(|e| format!(
+        "Unable to bind to {bind_address}: {e}"))?;
+
+    let app = Router::new()
+        .layer(middleware::from_fn(logging_middleware))
+        .route("/metrics", get(get_metrics))
+        .with_state(Arc::new(registry));
+
+    info!("[Metrics] Listening on {bind_address}.");
+    Ok(tokio::spawn(async move {
+        axum::serve(listener, app).await
+    }))
+}
+
+async fn get_metrics(State(registry): State<Arc<Registry>>) -> impl IntoResponse {
+    let mut result = String::new();
+
+    match prometheus_client::encoding::text::encode(&mut result, &registry) {
+        Ok(()) => {
+            Response::builder()
+                .status(StatusCode::OK)
+                .header(header::CONTENT_TYPE, "application/openmetrics-text; version=1.0.0; charset=utf-8")
+                .body(result)
+                .unwrap()
+        },
+        Err(err) => {
+            error!("[Metrics] Failed to collect metrics: {err}.");
+            Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .header(header::CONTENT_TYPE, "text/plain")
+                .body("Metrics collection error".to_owned())
+                .unwrap()
+        },
+    }
+}
+
+async fn logging_middleware(request: Request, next: Next) -> Response {
+    trace!("[Metrics] {} {}", request.method(), request.uri());
+    next.run(request).await
+}
